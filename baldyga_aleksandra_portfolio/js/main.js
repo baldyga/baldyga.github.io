e7e981b232a333896cb92629ea7b39e16026da68
Aleksandra <50842332+baldyga@users.noreply.github.com>
2019-09-12 22:08:06 +0200

    commit
    new file main.js

--- baldyga_aleksandra_portfolio/js/main.js
+++ baldyga_aleksandra_portfolio/js/main.js
@@ -1,12 +1,22 @@
 const times = document.querySelector('.fa-times');
 const bars = document.querySelector('.fa-bars');
+const btnCap = document.querySelector('.fa-graduation-cap');
+const btnClose = document.querySelector('.fa-times-circle')
 
-function burger() {
+function navBurger() {
     const nav = document.querySelector('nav');
 
     times.classList.toggle('hidden');
     bars.classList.toggle('hidden');
     nav.classList.toggle('hidden');
 }
-times.addEventListener('click', burger);
-bars.addEventListener('click', burger);
+times.addEventListener('click', navBurger);
+bars.addEventListener('click', navBurger);
+
+
+function moreInfo() {
+    const divMore = document.querySelector('.more');
+    divMore.classList.toggle('inactive');
+}
+btnCap.addEventListener('click', moreInfo);
+btnClose.addEventListener('click', moreInfo);
