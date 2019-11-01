e7005fd9a658dd8841233552a3b14b874fb2fe1d
Aleksandra <50842332+baldyga@users.noreply.github.com>
2019-11-01 19:19:21 +0100

    ready

--- staffordshireBullTerrier/js/main.js
+++ staffordshireBullTerrier/js/main.js
@@ -1,4 +1,12 @@
+// preloader
+const preloader = document.querySelector('section.wrapper');
+
+window.addEventListener('load', ()=> {
+    preloader.classList.add('preloader-hiding');
+});
+
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
